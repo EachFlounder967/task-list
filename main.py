2146b4dca49326e0274783df4079e4ccf4f79c12
EachFlounder967 <jarrad.m.billiot@gmail.com>
2026-03-25 16:36:55 -0500

    Created json format and added inital formatting display

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import sys
+import json
+
+with open("task-list.json", "r") as file:
+    data = json.load(file)
+tasks = data["tasks"]
+header = "{:<6} {:<25} {:<12}, {:<30}, {:<10}".format(
+    "Number", "Task", "Date Added", "Notes", "Priority"
+)
+
+
 def main():
     print("Hello from task-list!")
+    print(header)
+    print("-" * len(header))
+
+    for row in tasks:
+        print(
+            "{:<6} {:<25} {:<12}, {:<30}, {:<10}".format(
+                row["Number"],
+                row["Task"],
+                row["Date Added"],
+                row["Notes"],
+                row["Priority"],
+            )
+        )
 
 
 if __name__ == "__main__":
